f41b1177531f1fe49bf0b8a9e3daee48e7eb6bbe
VK <belsynth@gmail.com>
2018-10-17 11:25:20 +0300

    Добавил скрипт для отображения/скрытия меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,19 @@
-// null
+// ---------- Меню ----------
+
+var nav = document.querySelector('.navigation');
+var navToggle = document.querySelector('.navigation__logo-toggle');
+
+nav.classList.remove('navigation--nojs');
+nav.classList.add('navigation--closed');
+
+navToggle.addEventListener('click', function() {
+  if(nav.classList.contains('navigation--closed')) {
+    nav.classList.remove('navigation--closed');
+    navToggle.classList.remove('navigation__logo-toggle--open');
+    navToggle.classList.add('navigation__logo-toggle--close');
+  } else {
+    nav.classList.add('navigation--closed');
+    navToggle.classList.remove('navigation__logo-toggle--close');
+    navToggle.classList.add('navigation__logo-toggle--open');
+  }
+});
