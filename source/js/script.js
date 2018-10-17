5db30afc00b7f99de8de709fe837e80120e2c3cc
VK <belsynth@gmail.com>
2018-10-17 12:40:06 +0300

    Добавил скрипт вызова модального окна корзины

--- source/js/script.js
+++ source/js/script.js
@@ -6,8 +6,8 @@ var navToggle = document.querySelector('.navigation__logo-toggle');
 nav.classList.remove('navigation--nojs');
 nav.classList.add('navigation--closed');
 
-navToggle.addEventListener('click', function() {
-  if(nav.classList.contains('navigation--closed')) {
+navToggle.addEventListener('click', function () {
+  if (nav.classList.contains('navigation--closed')) {
     nav.classList.remove('navigation--closed');
     navToggle.classList.remove('navigation__logo-toggle--open');
     navToggle.classList.add('navigation__logo-toggle--close');
@@ -17,3 +17,35 @@ navToggle.addEventListener('click', function() {
     navToggle.classList.add('navigation__logo-toggle--open');
   }
 });
+
+// ---------- Корзина ----------
+
+var links = document.querySelectorAll('.js-basket');
+var popup = document.querySelector('.modal-basket');
+var overlay = document.querySelector('.overlay');
+
+
+links.forEach(function (element) {
+  element.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    overlay.classList.add('overlay--show');
+    popup.classList.add('modal--show');
+  });
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    overlay.classList.remove('overlay--show');
+    popup.classList.remove("modal--show");
+  }
+});
+
+if (overlay !== null) {
+  overlay.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    overlay.classList.remove('overlay--show');
+    popup.classList.remove("modal--show");
+  });
+}
