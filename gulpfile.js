ff44e36c8f0a7ca0d6ca3d44007b62b608388ada
VK <belsynth@gmail.com>
2018-10-26 15:23:02 +0300

    Добавил в guplfile.js watcher для отслеживания изменения js кода

--- gulpfile.js
+++ gulpfile.js
@@ -103,6 +103,7 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/sprite/*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("compress", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
